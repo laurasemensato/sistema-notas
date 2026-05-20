0b02ad1d75eb8763735f7a9d0ba985c510e08d75
Laura Nunes <laura.semensato@edu.senai.br>
2026-05-20 10:54:52 -0300

    feat: adiciona classificação e relatório

--- main.cpp
+++ main.cpp
@@ -48,5 +48,23 @@ cout << "\nAlunos cadastrados: " << endl;
 for (int i= 0; i < qtdAlunos; i++){
 cout << " " << i + 1 << ". " << nomes[i]<< endl;
 }
+cout << "\n=== RELATÓRIO ===" << endl;
+int aprovados = 0, recuperacao = 0, reprovados = 0;
+
+for (int i= 0; i < qtdAlunos; i++){
+    cout << nomes[i] << "-Media: " << media[i]<< " - ";
+    if (media[i]>= 7){
+        cout << "Aprovado" << endl;
+        aprovados++;
+    } else if (media[i]>= 5){
+    cout << "Recuperação" << endl;
+    recuperacao++;
+    } else {
+        cout << "Reprovados" << endl;
+        reprovados++;
+    }
+}
+
+cout << "\nResumo: " << aprovados << " aprovados: " << recuperacao << " em recuperacao: " << reprovados << " reprovados: " << endl;
     return 0;
 }
