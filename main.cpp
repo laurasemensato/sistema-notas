a0ea7c991577b800f3c153596f9babcd4a69df32
Laura Nunes <laura.semensato@edu.senai.br>
2026-05-20 10:30:29 -0300

    feat: adiciona notas e cálculos de média

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main (){
 //Entrada - declaração das variáveis
 string nomes[20];
 int qtdAlunos;
+float notas[20][5];
+float media [20];
+int qtdDisciplinas;
 
 //Processamento 
 cout << "=== SISTEMA DE NOTAS v4.0 ===" << endl;
@@ -22,6 +25,24 @@ for (int i = 0; i < qtdAlunos; i++){
     cout << "Nome do aluno: " << i + 1 << ":";
     getline(cin,nomes[i]);
 }
+do {
+    cout << "\nQuantidade de disciplinas (1 a 5): ";
+    cin >> qtdDisciplinas;
+} while (qtdDisciplinas < 1 || qtdDisciplinas > 5); 
+
+for (int i= 0; i < qtdAlunos; i++){
+    cout << "\nNotas de " << nomes[i] << ":" << endl;
+    float soma = 0;
+    for (int j= 0; j < qtdDisciplinas; j++){
+do {
+    cout << "Disciplina " << j + 1 << "(1 a 5): ";
+    cin >> notas[i][j];
+    } while (notas[i][j] < 0 || notas [i][j] > 10);
+    soma += notas[i][j];
+}
+media [i]= soma / qtdDisciplinas;
+}
+
 //Saida
 cout << "\nAlunos cadastrados: " << endl;
 for (int i= 0; i < qtdAlunos; i++){
